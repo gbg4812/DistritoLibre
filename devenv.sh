3864e64e3ea05930966f84a46c3f1e6148795a85
Guillem Baldi <guillembaldi@gmail.com>
2025-06-11 10:21:19 +0200

    last fix

---
+++ devenv.sh
@@ -0,0 +1,4 @@
+wezterm cli spawn --cwd . -- zsh -c "source .venv/bin/activate && python manage.py runserver"
+wezterm cli spawn --cwd ./distrito_libre_frontend/ -- zsh -c "npm run dev"
+source .venv/bin/activate 
+nvim .
